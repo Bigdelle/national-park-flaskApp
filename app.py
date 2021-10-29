dfeed4966827e6ee805741c541cc02e3a932d6a6
Benjamin Bigdelle <brb227@cornell.edu>
2021-10-29 15:06:25 -0400

    Added web links

--- app.py
+++ app.py
@@ -5,7 +5,16 @@ import json
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
+    return render_template('index.html')
+
+@app.route("/index")
+def home_index():
+    return render_template('index.html')
+
+
+@app.route('/activities')
+def index():
     req = requests.get('https://developer.nps.gov/api/v1/activities/parks?parkCode=&api_key=3wguztEg5MM7UMGZI7jFbo2cBBhXvUq30k53GJHV')
     data = json.loads(req.content)
-    return render_template('index.html', data=data)
+    return render_template('activities.html', data=data)
